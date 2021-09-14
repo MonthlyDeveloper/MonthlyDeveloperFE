730a6b2d6d376bbc56a7bc3561dc878a68569d0f
lavegaa <cksgh405@naver.com>
2021-09-14 17:23:33 +0900

    config: storybook styled-component 및 global style설정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,17 @@
 const path = require("path");
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "storybook-addon-styled-component-theme/dist/preset",
+    "@storybook/addon-actions",
+    "@storybook/addon-controls",
   ],
   webpackFinal: async (config) => {
     config.resolve.modules = [
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,13 +6,13 @@
 //       date: /Date$/,
 //     },
 //   },
-// }
+// };
 
 import { addDecorator } from "@storybook/react";
 import { withThemesProvider } from "storybook-addon-styled-component-theme";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../styles/theme";
-import { GlobalStyle } from "../styles/GlobalStyles";
+import GlobalStyle from "../styles/GlobalStyle";
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: { matchers: { color: /(background|color)$/i, date: /Date$/ } },
