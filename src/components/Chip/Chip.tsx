ab90c09591178bba17e43670435797aa2729883b
lavegaa <cksgh405@naver.com>
2021-09-15 16:47:49 +0900

    ✨ ADD: Chip기본사항 제작(#2)

---
+++ src/components/Chip/Chip.tsx
@@ -0,0 +1,43 @@
+import { Container, DeleteButton } from "./Chip.style";
+
+interface ChipProps {
+  /** Chip value */
+  text?: string;
+  /** normal or editing */
+  type?: "normal" | "editing";
+  /** Editing function */
+  handleDelete?: () => void;
+  /** Chip background color */
+  bgColor?: string;
+  /** Chip text color */
+  txtColor?: string;
+  /** Add style if you want */
+  addStyle?: any;
+}
+
+const Chip = ({
+  text,
+  type,
+  handleDelete,
+  bgColor,
+  txtColor,
+  addStyle,
+}: ChipProps) => {
+  return (
+    <Container bgColor={bgColor} addStyle={addStyle} txtColor={txtColor}>
+      {text}
+      {type === "editing" && (
+        <DeleteButton onClick={handleDelete}>X</DeleteButton>
+      )}
+    </Container>
+  );
+};
+
+Chip.defaultProps = {
+  text: "Sample",
+  type: "normal",
+  bgColor: "white",
+  txtColor: "black",
+};
+
+export default Chip;
